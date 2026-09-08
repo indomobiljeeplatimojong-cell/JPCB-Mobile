7c99327b4cbc06e91deb534d3d1e0728c96531a8
indomobiljeeplatimojong-cell <indomobiljeeplatimojong@gmail.com>
2026-09-08 12:53:48 +0800

    Update MainActivity.java

--- app/src/main/java/com/jpcb/mobile/MainActivity.java
+++ app/src/main/java/com/jpcb/mobile/MainActivity.java
@@ -1,22 +1,19 @@
 package com.jpcb.mobile;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import androidx.activity.OnBackPressedCallback;
-import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     private static final String APP_URL =
             "https://script.google.com/macros/s/AKfycbwvJlMz3-5hrAFj20HZaiqaucZaEMbmUerEjue-LjzD83r_ZfoNTNfzC1ZLEeVG2Hg8Eg/exec?action=mobile";
-
-    private WebView webView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -24,36 +21,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         webView = new WebView(this);
-        webView.setBackgroundColor(Color.WHITE);
         setContentView(webView);
 
         WebSettings settings = webView.getSettings();
+
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
         settings.setLoadsImagesAutomatically(true);
+
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
+
         settings.setJavaScriptCanOpenWindowsAutomatically(true);
         settings.setSupportMultipleWindows(false);
+
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
-        settings.setUserAgentString(settings.getUserAgentString() + " JPCBMobile/1.0");
+
+        settings.setUserAgentString(
+                settings.getUserAgentString() + " JPCBMobile/2.0"
+        );
 
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
-        webView.loadUrl(APP_URL);
 
-        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
-            @Override
-            public void handleOnBackPressed() {
-                if (webView != null && webView.canGoBack()) {
-                    webView.goBack();
-                } else {
-                    finish();
-                }
-            }
-        });
+        webView.loadUrl(APP_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -66,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
             webView.destroy();
             webView = null;
         }
+
         super.onDestroy();
     }
 }
